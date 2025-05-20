55d852054d4cd17155639c731e4cf30f484dbd2d
shubhamagr24 <shubhamagr24@gmil.com>
2025-05-21 00:25:47 +0530

    updated vector.py

--- vector.py
+++ vector.py
@@ -8,35 +8,36 @@ df=pd.read_csv("realistic_restaurant_reviews.csv")
 
 embeddings=OllamaEmbeddings(model="nomic-embed-text:latest")
 
-# Create a Chroma vector store
+db_location = "./chrome_langchain_db"
+add_documents = not os.path.exists(db_location)
 
-db_location="./chroma_db"
-
+if add_documents:
+    documents = []
+    ids = []
+    
+    for i, row in df.iterrows():
+        document = Document(
+            page_content=row["Title"] + " " + row["Review"],
+            metadata={"rating": row["Rating"], "date": row["Date"]},
+            id=str(i)
+        )
+        ids.append(str(i))
+        documents.append(document)
+        
 vector_store = Chroma(
     collection_name="restaurant_reviews",
     persist_directory=db_location,
     embedding_function=embeddings
 )
 
-add_documents = not os.path.exists(db_location)
-
-
-
 if add_documents:
-    documents=[]
-    ids=[]
-
-    for i,row in df.iterrows():
-        documents.append(Document(
-            page_content=row["review"],
-            metadata={"date":row["Date"],"rating":row["Rating"]}
-            ))
-        ids.append(str(i))
-
-
     vector_store.add_documents(documents=documents, ids=ids)
+    # print("added :",vector_store._collection.count(), "documents")
+
     
 retriever = vector_store.as_retriever(
-    search_kwargs={"k": 3}
+    search_kwargs={"k": 5}
 )
+
+
 
