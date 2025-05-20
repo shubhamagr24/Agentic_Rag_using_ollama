72dffb6715e30bd1c98333d2bf41e73e1d85a96f
shubhamagr24 <shubhamagr24@gmil.com>
2025-05-21 00:25:23 +0530

    added retriver to app

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from vector import retriever
 
 model = OllamaLLM(model="deepseek-r1:1.5b", temperature=0.2)
 
 
 prompt = ChatPromptTemplate.from_template("""
 You are a helpful assistant. You will be given a question and you need to answer it in detail.
+Below is the context to help you answer the question.
+{context}
 Question: {question}
 Answer: 
 """)
@@ -16,6 +19,14 @@ parser=StrOutputParser()
 # Create the chain
 chain = prompt | model | parser
 
-print(chain.invoke({"question":"Key Difference between Rag And Agaentic Rag"}))
-
+while True:
+    print("\n\n-------------------------------")
+    question = input("Ask your question (q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    context = retriever.invoke(question)
+    result = chain.invoke({"context": context, "question": question})
+    print(result)
 
